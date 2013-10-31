e2fce7f2cf023d56300ee8822a2f6f8ea457c2c3
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2013-10-30 23:33:49 -0400

    Remove absolute links

--- blog-src/Gruntfile.js
+++ blog-src/Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
         options: {
           pageSrc: 'src/pages',
           data: {
-            baseUrl: '/'
+            baseUrl: ''
           },
           pagination: {
             postsPerPage: 2,
